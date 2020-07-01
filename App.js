7f89f47de56170dc0263a363e1dfbaff9ce433e6
Andrew <andrew.barefootgorilla@gmail.com>
2020-07-01 17:49:10 +1000

    Created Repo

--- App.js
+++ App.js
@@ -1,21 +1,44 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { MapView, Permissions, Location } from "expo";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      region: {
+        latitude: 37.78825,
+        longitude: -122.4324,
+        latitudeDelta: 0.922,
+        longitudeDelta: 0.0421,
+      }
+    }
+  }
+
+  _getLocationAsync = async () => {
+    let { status } = await Permissions.askAsync(Permissions.LOCATION)
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Home Screen</Text>
+        <MapView
+          initialRegion={this.state.region}
+          showUserLocation={true}
+          showsCompass={true}
+          rotateEnabled={false}
+          style={{ flex: 1 }}
+        />
+        </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
   },
 });
